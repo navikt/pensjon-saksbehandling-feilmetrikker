0930d19acf4650a1c35c32eff95cbda333f9bb5a
Marius Eriksen <marius.eriksen@nav.no>
2019-12-12 10:35:55 +0100

    Changed function name for demo

--- src/main/kotlin/no/nav/pensjon/saksbehandling/database/Database.kt
+++ src/main/kotlin/no/nav/pensjon/saksbehandling/database/Database.kt
@@ -5,18 +5,19 @@ import javax.sql.DataSource
 
 internal class Database(private val dataSource: DataSource) {
     private companion object {
-        private const val COUNT_AVVIKSINFORMASJON_PSAK = """SELECT COUNT(1) FROM PEN.T_AVVIKSINFORMASJON WHERE APPLIKASJON = 'PSAK'"""
-        private const val COUNT_AVVIKSTILFELLER_PSAK = """ SELECT COUNT(1) FROM PEN.T_AVVIKSINFORMASJON i
+        private const val COUNT_AVVIKSINFORMASJON_PSAK =
+            "SELECT COUNT(1) FROM PEN.T_AVVIKSINFORMASJON WHERE APPLIKASJON = 'PSAK'"
+        private const val COUNT_AVVIKSTILFELLER_PSAK = """SELECT COUNT(1) FROM PEN.T_AVVIKSINFORMASJON i
                 INNER JOIN PEN.T_AVVIKSGRUPPE g on g.AVVIKSINFORMASJON_ID = i.AVVIKSINFORMASJON_ID
                 INNER JOIN PEN.T_AVVIKSTILFELLE t on t.AVVIKSGRUPPE_ID = g.AVVIKSGRUPPE_ID
                 WHERE i.APPLIKASJON = 'PSAK'"""
     }
 
-    internal fun countAvviksinformasjonFromPsak() = countTechnicalErrorsFromPsak(COUNT_AVVIKSINFORMASJON_PSAK)
+    internal fun countAvviksinformasjonFromPsak() = countQuery(COUNT_AVVIKSINFORMASJON_PSAK)
 
-    internal fun countAvvikstilfellerFromPsak() = countTechnicalErrorsFromPsak(COUNT_AVVIKSTILFELLER_PSAK)
+    internal fun countAvvikstilfellerFromPsak() = countQuery(COUNT_AVVIKSTILFELLER_PSAK)
 
-    private fun countTechnicalErrorsFromPsak(query: String): Double = try {
+    private fun countQuery(query: String): Double = try {
         dataSource.connection.use {
             val response = it.prepareStatement(query).executeQuery()
             response.next()
